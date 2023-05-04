cd203aafba6bdc3d01ba5c3c79082e8e17251169
dorratounsy <75041455+dorratounsy@users.noreply.github.com>
2023-05-04 19:55:57 +0100

    Update reservation.model.js

--- BackBlasti/models/reservation.model.js
+++ BackBlasti/models/reservation.model.js
@@ -1,1 +1,20 @@
 
+const mongoose = require("mongoose");
+
+const reservationSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    isAccepted: {
+        type: Bool,
+        required: 0,
+    }
+
+  
+});
+
+const reservations = mongoose.model("reservations", reservationsSchema);
+
+module.exports = { reservations };
